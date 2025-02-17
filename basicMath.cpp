5171c9816080e6e084daf14dd1b9bfa256682634
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-02-17 23:38:53 +0000

    Added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2025 Atri Sarker All rights reserved.
+// .
+// Created By: Atri S
+// Date: Feb 17, 2025
+// Basic Math Operators in C++
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "88+22 = " << (88 + 22) << std::endl;  // Addition
+    std::cout << "1-100 = " << (1 - 100) << std::endl;  // Subtraction
+    std::cout << "45*267 = " << (45 * 267) << std::endl;  // Multiplication
+    std::cout << "60/5 = " << (60 / 5) << std::endl;  // Division
+    std::cout << "2\u2074 = " << (pow(2, 4)) << std::endl;  // Exponentiation
+    std::cout << "\u221a81 = " << (sqrt(81)) << std::endl;  // Square Root
+    std::cout << "89//9 = " << (floor(89 / 9)) << std::endl;  // Floor Division
+    std::cout << "mod\u2083(10) = " << (10 % 3) << std::endl;  // Modulo
+}
